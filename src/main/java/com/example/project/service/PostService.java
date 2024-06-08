bc295ba305ce78ec887daae4f452b0a4176fc5a2
thun0514 <xogns2301@gmail.com>
2024-06-09 00:28:40 +0900

    Feat: Post Service 껍데기 생성

---
+++ src/main/java/com/example/project/service/PostService.java
@@ -0,0 +1,44 @@
+package com.example.project.service;
+
+
+import com.example.project.domain.Post;
+import com.example.project.dto.PostRequestDto;
+import com.example.project.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+    private final PostRepository postRepository;
+
+    @Transactional
+    public Post createPost(PostRequestDto requestDto) {
+        Post post = new Post(requestDto);
+        postRepository.save(post);
+        return post;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Post> getPosts() {
+        return postRepository.findAllByOrderByModifiedAtDesc();
+    }
+
+    @Transactional
+    public Long update(Long id, PostRequestDto requestDto) {
+        Post post = postRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("게시글이 존재하지 않습니다.")
+        );
+        post.update(requestDto);
+        return post.getPostNo();
+    }
+
+    @Transactional
+    public Long deletePost(Long id) {
+        postRepository.deleteById(id);
+        return id;
+    }
+}
