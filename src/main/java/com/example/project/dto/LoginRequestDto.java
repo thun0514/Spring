e026f6f1209a4b41a852fa19d175027b111c7505
thun0514 <xogns2301@gmail.com>
2024-06-09 01:00:26 +0900

    Feat: 회원 정보 관리 중 로그인을 위한 Login Dto 생성

---
+++ src/main/java/com/example/project/dto/LoginRequestDto.java
@@ -0,0 +1,11 @@
+package com.example.project.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequestDto {
+    private String userId;
+    private String password;
+}
