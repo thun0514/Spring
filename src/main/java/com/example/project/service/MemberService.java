0c1b8327101c268a8a56c6131bd5fb131053921a
thun0514 <xogns2301@gmail.com>
2024-06-09 00:54:43 +0900

    Feat: Member Service에서 회원 가입을 위한 객체 추가

---
+++ src/main/java/com/example/project/service/MemberService.java
@@ -0,0 +1,39 @@
+package com.example.project.service;
+
+
+import com.example.project.domain.Member;
+import com.example.project.dto.SignupRequestDto;
+import com.example.project.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public void signup(SignupRequestDto signupRequestDto) {
+        String userId = signupRequestDto.getUserId();
+        String password = signupRequestDto.getPassword();
+        String email = signupRequestDto.getEmail();
+        String userName = signupRequestDto.getUserName();
+
+        Optional<Member> foundId = memberRepository.findByUserId(userId);
+        Optional<Member> foundEmail = memberRepository.findByEmail(email);
+
+        if (foundId.isPresent()) {
+            throw new IllegalArgumentException("이미 존재하는 ID 입니다.");
+        }
+
+        if (foundEmail.isPresent()) {
+            throw new IllegalArgumentException("이미 가입된 E-Mail 입니다.");
+        }
+
+        Member member = new Member(userId, password, userName, email);
+        memberRepository.save(member);
+    }
+}
