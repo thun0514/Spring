f040c3ef23529192215b1057ac846a3b35b2ce7a
thun0514 <xogns2301@gmail.com>
2024-06-09 00:29:26 +0900

    Feat: Post Service에 게시글 조회를 위한 getPosts 구현

--- src/main/java/com/example/project/service/PostService.java
+++ src/main/java/com/example/project/service/PostService.java
@@ -2,7 +2,6 @@ package com.example.project.service;
 
 
 import com.example.project.domain.Post;
-import com.example.project.dto.PostRequestDto;
 import com.example.project.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,30 +14,8 @@ import java.util.List;
 public class PostService {
     private final PostRepository postRepository;
 
-    @Transactional
-    public Post createPost(PostRequestDto requestDto) {
-        Post post = new Post(requestDto);
-        postRepository.save(post);
-        return post;
-    }
-
     @Transactional(readOnly = true)
     public List<Post> getPosts() {
         return postRepository.findAllByOrderByModifiedAtDesc();
-    }
-
-    @Transactional
-    public Long update(Long id, PostRequestDto requestDto) {
-        Post post = postRepository.findById(id).orElseThrow(
-                () -> new IllegalArgumentException("게시글이 존재하지 않습니다.")
-        );
-        post.update(requestDto);
-        return post.getPostNo();
-    }
-
-    @Transactional
-    public Long deletePost(Long id) {
-        postRepository.deleteById(id);
-        return id;
     }
 }
