4addb70097add3c6343f9a5b0395260acc9894f8
thun0514 <xogns2301@gmail.com>
2024-06-11 08:50:12 +0900

    Fix: 예외처리를 Service 단에서 처리하게끔 이동 및 Client에게 반환해주는 타입 변경

--- src/main/java/com/example/project/controller/PostController.java
+++ src/main/java/com/example/project/controller/PostController.java
@@ -1,35 +1,60 @@
 package com.example.project.controller;
 
-import com.example.project.domain.Post;
 import com.example.project.dto.PostRequestDto;
+import com.example.project.dto.PostResponseDto;
+import com.example.project.security.JwtUtil;
 import com.example.project.service.PostService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/posts")
 public class PostController {
     private final PostService postService;
+    private final JwtUtil jwtUtil;
 
-    @GetMapping("/posts")
-    public List<Post> getPosts() {
-        return postService.getPosts();
+    // 전체 게시글 조회
+    @GetMapping
+    public ResponseEntity<List<PostResponseDto>> getPosts() {
+        return ResponseEntity.ok(postService.getPosts());
     }
 
-    @PostMapping("/posts/update")
-    public Post creatPost(@RequestBody PostRequestDto requestDto) {
-        return postService.createPost(requestDto);
+    // 선택한 게시글 조회
+    @GetMapping("/{id}")
+    public ResponseEntity<PostResponseDto> getPostById(@PathVariable("id") Long id, HttpServletRequest servletRequest) {
+        PostResponseDto responseDto = postService.getPostById(id, servletRequest);
+        return ResponseEntity.ok(responseDto);
     }
 
-    @PutMapping("/posts/update")
-    public Long updatePost(@RequestParam("id") Long id, @RequestBody PostRequestDto requestDto) {
-        return postService.update(id, requestDto);
+    // 게시글 업로드
+    @PostMapping("/update")
+    public ResponseEntity<PostResponseDto> createPost(@RequestBody PostRequestDto requestDto, HttpServletRequest servletRequest) {
+        PostResponseDto responseDto = postService.createPost(requestDto, servletRequest);
+        return ResponseEntity.ok(responseDto);
     }
 
-    @DeleteMapping("/posts/delete")
-    public Long deletePost(@RequestParam("id") Long id) {
-        return postService.deletePost(id);
+    // 선택한 게시글 수정
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Long> updatePost(@PathVariable("id") Long id, @RequestBody PostRequestDto requestDto, HttpServletRequest servletRequest) {
+        Long updatePostId = postService.update(id, requestDto, servletRequest);
+        return ResponseEntity.ok(updatePostId);
+    }
+
+    // 선택한 게시글 삭제
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Map<String, Object>> deletePost(@PathVariable("id") Long id, HttpServletRequest servletRequest) {
+        postService.deletePost(id, servletRequest);
+
+        Map<String, Object> userResponse = new HashMap<>();
+        userResponse.put("status", HttpStatus.OK.value());
+        return new ResponseEntity<>(userResponse, HttpStatus.OK);
     }
 }
