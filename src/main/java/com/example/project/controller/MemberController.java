6ef4a66b73e5ebb3baae8761e25a569ac2655f24
thun0514 <xogns2301@gmail.com>
2024-06-11 08:59:31 +0900

    Fix: 상태코드와 메세지도 함께 반환하도록 변경

--- src/main/java/com/example/project/controller/MemberController.java
+++ src/main/java/com/example/project/controller/MemberController.java
@@ -6,10 +6,15 @@ import com.example.project.dto.SignupRequestDto;
 import com.example.project.service.MemberService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,15 +22,23 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/signup")
-    public String signup(@RequestBody SignupRequestDto signupRequestDto) {
+    public ResponseEntity<Map<String, Object>> signup(@RequestBody SignupRequestDto signupRequestDto) {
         memberService.signup(signupRequestDto);
-        return "redirect:/login";
+
+        Map<String, Object> userResponse = new HashMap<>();
+        userResponse.put("message", "회원가입 완료");
+        userResponse.put("status", HttpStatus.OK.value());
+        return new ResponseEntity<>(userResponse, HttpStatus.OK);
     }
 
     @ResponseBody
     @PostMapping("/login")
-    public String login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
+    public ResponseEntity<Map<String, Object>> login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
         memberService.login(loginRequestDto, response);
-        return "success";
+
+        Map<String, Object> userResponse = new HashMap<>();
+        userResponse.put("message", "로그인 완료");
+        userResponse.put("status", HttpStatus.OK.value());
+        return new ResponseEntity<>(userResponse, HttpStatus.OK);
     }
 }
