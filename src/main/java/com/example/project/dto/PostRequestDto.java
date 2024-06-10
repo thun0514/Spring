60897f46eaf4e9ed76629316125c62fa32ae12dd
thun0514 <xogns2301@gmail.com>
2024-06-11 08:45:55 +0900

    Fix: 필요없는 어노테이션 제거

--- src/main/java/com/example/project/dto/PostRequestDto.java
+++ src/main/java/com/example/project/dto/PostRequestDto.java
@@ -1,10 +1,8 @@
 package com.example.project.dto;
 
 import lombok.Data;
-import lombok.Getter;
 
 @Data
-@Getter
 public class PostRequestDto {
     private String title;
     private String contents;
--- src/main/java/com/example/project/dto/SignupRequestDto.java
+++ src/main/java/com/example/project/dto/SignupRequestDto.java
@@ -2,12 +2,8 @@ package com.example.project.dto;
 
 import com.example.project.domain.RoleType;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
-@Getter
-@Setter
 public class SignupRequestDto {
     private String userId;
     private String password;
